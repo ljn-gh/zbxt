7531e8c70531d2f35b247469b81ab7a73254fee8
ljn <ljn_email@163.com>
2020-05-06 11:04:00 +0800

    都是

--- src/main/java/com/ljnan/zbxt/controller/TestController.java
+++ src/main/java/com/ljnan/zbxt/controller/TestController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -17,6 +18,7 @@ import java.util.Arrays;
 @RestController
 @Slf4j
 @Api("TestController")
+@RequestMapping("test")
 public class TestController {
     @Autowired
     private TestService testService;
